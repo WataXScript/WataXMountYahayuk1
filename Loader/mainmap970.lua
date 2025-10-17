ec6058c62f0a611f9fc5d7f167fa40856f512bf0
WataXScript <dalvo9216@gmail.com>
2025-10-18 03:30:30 +0800

    Update mainmap970.lua

--- Loader/mainmap970.lua
+++ Loader/mainmap970.lua
@@ -110,7 +110,7 @@ local function startMovement() isMoving = true end
 local function stopMovement() isMoving = false end
 
 
-local DEFAULT_HEIGHT = 2.9
+local DEFAULT_HEIGHT = 6.4
 local function getCurrentHeight()
     local char = player.Character or player.CharacterAdded:Wait()
     local humanoid = char:WaitForChild("Humanoid")
